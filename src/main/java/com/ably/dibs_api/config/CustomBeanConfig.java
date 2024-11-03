e5b1667ecff200383d3661eead6f877d4ce39063
KDH <eogh6428@naver.com>
2024-11-04 04:23:25 +0900

    redis config

--- src/main/java/com/ably/dibs_api/config/CustomBeanConfig.java
+++ src/main/java/com/ably/dibs_api/config/CustomBeanConfig.java
@@ -3,8 +3,12 @@ package com.ably.dibs_api.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -27,6 +31,22 @@ public class CustomBeanConfig {
                 .title("Ably DibsAPI Swagger")
                 .description("DibsAPI REST API")
                 .version("1.0.0");
+    }
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+
+        configuration.setHostName(host);
+        configuration.setPort(port);
+
+        return new LettuceConnectionFactory(configuration);
     }
 
 }
